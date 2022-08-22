ffe2f6bc1bfd2707aad07b429f3c5d7955576550
luelisan98 <111664893+luelisan98@users.noreply.github.com>
2022-08-21 18:23:11 -0600

    Agregar Arte al juego y escenas que hacían falta

--- Mink/Assets/Scripts/SlotsHandler.cs
+++ Mink/Assets/Scripts/SlotsHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fef20eda589f2415a594464c4892ac753e3643ea6718bd47484e4c0d46e73215
-size 887
+oid sha256:5e46620c03c0e9767db01e4e51c8740a2489cc8f5a6a7f68720d60e7047dbdb5
+size 1061
